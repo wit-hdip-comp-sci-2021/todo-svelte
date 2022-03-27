a3eec4cd70369f2ce670e36ec28b741bbe1e43e7
Eamonn de Leastar <edeleastar@wit.ie>
2022-03-27 17:28:50 +0100

    replace uuid function with import from library

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,9 @@
 <script>
+  import { v4 as uuidv4 } from 'uuid';
+
   let todoText = "";
   let todoItems = [];
   let doneItems = [];
-
-  function uuidv4() {
-    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-      var r = Math.random() * 16 | 0, v = c == "x" ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
-    });
-  }
 
   function addTodo() {
     const todo = {
